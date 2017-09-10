25c48fe5b11f213ae858d78774cf2142bb720600
NinoMaj <nino.majder@gmail.com>
2017-09-10 23:37:44 +0200

    Final

--- client/src/actions/pokemonActions.js
+++ client/src/actions/pokemonActions.js
@@ -20,13 +20,17 @@ export const getPokemons = offset => (dispatch) => {
   }
 
   function getPokemonsFromAPI() {
+    // getting list of pokemons, limit = how many are we fetching, offset = continue fetching for last loaded pokemon (initially 0)
     return fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${pokemonsLoadedPerRequest}&offset=${offset}`, { method: 'GET' })
       .then(resp => resp.json()).then((pokemonsResponse) => {
+        // grabbing pokemons details with 2. API call for all pokemons catched with 1. API call
         const promises = [];
         for (let i = 0; i < pokemonsResponse.results.length; i += 1) {
           promises.push(getPokemonDetail(pokemonsResponse.results[i].name));
         }
+
         Promise.all(promises)
+        // continue only when all pokemons details are fetched
           .then((pokemonsWithDetails) => {
             function pokemonIdWithZerosPadding(id) {
               if (id < 10) {
@@ -36,7 +40,8 @@ export const getPokemons = offset => (dispatch) => {
               }
               return id;
             }
-
+            
+            // transforming data from API, taking only neccessary data for app store/state
             const dataForState = pokemonsWithDetails.map((pokemon) => {
               const type = pokemon.types.map(item => item.type.name);
               const stats = pokemon.stats.map((item, i) => ({
@@ -48,7 +53,7 @@ export const getPokemons = offset => (dispatch) => {
                 id: pokemon.id,
                 name: pokemon.name,
                 description: `This is a dummy description for pokemon ${pokemon.name}`,
-                imgUrl: pokemon.sprites.front_default,
+                imgUrl: pokemon.sprites.front_default, // this is currently not used
                 largeImgUrl: `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemonIdWithZerosPadding(pokemon.id)}.png`,
                 height: pokemon.height,
                 weight: pokemon.weight,
@@ -56,6 +61,8 @@ export const getPokemons = offset => (dispatch) => {
                 stats,
               };
             });
+            // dispatching action with 1.) next URL (this is not used currently), 2.) offset so we can know id of last loaded pokemon
+            // so we can continue fetching from that pokemon id, and finally 3.) data that we need for our app
             dispatch(getPokemonsSuccess({ next: pokemonsResponse.next, offset: pokemonsLoadedPerRequest, data: dataForState }));
           })
           .catch((err) => {
