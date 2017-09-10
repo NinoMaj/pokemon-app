3b84fe17610868fc9bf789514ff400c8dccbc32b
NinoMaj <nino.majder@gmail.com>
2017-09-10 09:23:35 +0200

    Everything done, except final polish

--- passport/auth.js
+++ passport/auth.js
@@ -6,17 +6,17 @@ const callbackPath = isProd ?
 
 module.exports = {
   twitterAuth: {
-    consumerKey: isProd ? '1OfRIUy70ChhM0zwhd59oDGsO' : 'pDgW2qdfYmE7siYxboN5esjsX',
+    consumerKey: isProd ? 'xxx' : 'xxx',
     callbackURL: `${callbackPath}auth/twitter/callback`,
   },
 
   googleAuth: {
-    clientID: '566331442205-7uip1rjq3kb12kcjd3r458qbjbv3tri4.apps.googleusercontent.com',
+    clientID: 'xxx',
     callbackURL: `${callbackPath}auth/google/callback`,
   },
 
   githubAuth: {
-    clientID: isProd ? 'c44d960c8b1e4fa06dbf' : '268f2bef895718508c27',
+    clientID: isProd ? 'xxx' : 'xxx',
     callbackURL: `${callbackPath}auth/github/callback`,
   },
 };
--- passport/index.js
+++ passport/index.js
@@ -1,6 +1,6 @@
-// import TwitterStrategy from 'passport-twitter'
+import TwitterStrategy from 'passport-twitter';
 import GoogleStrategy from 'passport-google-oauth';
-// import GitHubStrategy from 'passport-github2'
+import GitHubStrategy from 'passport-github2';
 
 import User from '../models/user';
 import configAuth from './auth';
@@ -12,33 +12,33 @@ const isProd = process.env.NODE_ENV === 'production' ? true : false;
 // parameter: New user/account ('new') or link new account to existing user ('link').
 // [service] is the social service used: 'twitter', 'google', 'github'
 const addUserToDB = (action, service, req, token, profile, done) => {
-  const user = action === 'new' ? new User() : req.user
-  user[service].id = profile.id // sets the user's social service id.
-  user[service].token = token // saves the token provided to the user
-  user[service].displayName = profile.displayName // saves displayName and username
-  user[service].username = profile.username
+  const user = action === 'new' ? new User() : req.user;
+  user[service].id = profile.id; // sets the user's social service id.
+  user[service].token = token; // saves the token provided to the user
+  user[service].displayName = profile.displayName; // saves displayName and username
+  user[service].username = profile.username;
 
   if (!user.userName) { // Sets userName in User profile
-    user.userName = profile.username
+    user.userName = profile.username;
   }
 
   // save our user to the database
   user.save((err) => {
     if (err) throw err
-    return done(null, user)
+    return done(null, user);
   })
 }
 
 // Function to manage authentication response and save user to database.
 const passportCallback = (service, req, token, refreshToken, profile, done) => {
   process.nextTick(() => {
-    const serviceID = `${service}.id`
+    const serviceID = `${service}.id`;
     if (!req.user) { // if not user is logged in
       // find the user in the database based on their service id
       User.findOne({ [serviceID]: profile.id }, (err, user) => {
         // if there is an error, stop everything and return that
         // for example an error connecting to the database
-        if (err) return done(err)
+        if (err) return done(err);
         // if the user is found, then log him/her in
         if (user) {
           // if there is a user id already but no token (user was linked at one
@@ -50,14 +50,14 @@ const passportCallback = (service, req, token, refreshToken, profile, done) => {
             addToUser[service].username = profile.username;
 
             user.save((err) => {
-              if (err) throw err
+              if (err) throw err;
               return done(null, user);
             });
           }
           return done(null, user); // user found, return that user
         }
         // if there is no user found with that service id, create a new one
-        addUserToDB('new', service, req, token, profile, done)
+        addUserToDB('new', service, req, token, profile, done);
       });
       // user already exists and is logged in, we have to link accounts
     } else {
@@ -85,16 +85,16 @@ module.exports = (passport) => {
     });
   });
 
-  // passport.use(new TwitterStrategy({
-  //   // pull in our app id from auth.js file and secret from .env file.
-  //   consumerKey: configAuth.twitterAuth.consumerKey,
-  //   consumerSecret: isProd ? process.env.TWITTER_SECRET : process.env.TWITTER_SECRET_DEV,
-  //   callbackURL: configAuth.twitterAuth.callbackURL,
-  //   passReqToCallback: true,
-  // },
-  //   (req, token, refreshToken, profile, done) => {
-  //     passportCallback('twitter', req, token, refreshToken, profile, done)
-  //   }))
+  passport.use(new TwitterStrategy({
+    // pull in our app id from auth.js file and secret from .env file.
+    consumerKey: configAuth.twitterAuth.consumerKey,
+    consumerSecret: isProd ? process.env.TWITTER_SECRET : process.env.TWITTER_SECRET_DEV,
+    callbackURL: configAuth.twitterAuth.callbackURL,
+    passReqToCallback: true,
+  },
+    (req, token, refreshToken, profile, done) => {
+      passportCallback('twitter', req, token, refreshToken, profile, done);
+    }));
 
   passport.use(new GoogleStrategy.OAuth2Strategy({
     clientID: configAuth.googleAuth.clientID,
@@ -106,13 +106,13 @@ module.exports = (passport) => {
       passportCallback('google', req, token, refreshToken, profile, done);
     }));
 
-  // passport.use(new GitHubStrategy({
-  //   clientID: configAuth.githubAuth.clientID,
-  //   clientSecret: isProd ? process.env.GITHUB_SECRET : process.env.GITHUB_SECRET_DEV,
-  //   callbackURL: configAuth.githubAuth.callbackURL,
-  //   passReqToCallback: true,
-  // },
-  //   (req, token, refreshToken, profile, done) => {
-  //     passportCallback('github', req, token, refreshToken, profile, done)
-  //   }))
+  passport.use(new GitHubStrategy({
+    clientID: configAuth.githubAuth.clientID,
+    clientSecret: isProd ? process.env.GITHUB_SECRET : process.env.GITHUB_SECRET_DEV,
+    callbackURL: configAuth.githubAuth.callbackURL,
+    passReqToCallback: true,
+  },
+    (req, token, refreshToken, profile, done) => {
+      passportCallback('github', req, token, refreshToken, profile, done);
+    }));
 };
